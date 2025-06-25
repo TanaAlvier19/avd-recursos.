6fb2d7e0906c4273f3805924df482f3f1f30fd98
TanaAlvier19 <tanaalvier@gmail.com>
2025-06-25 22:18:27 +0100

    Update layout.tsx

--- src/app/(dentro)/(dashboard)/layout.tsx
+++ src/app/(dentro)/(dashboard)/layout.tsx
@@ -3,25 +3,39 @@ import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
-import {useState} from "react"
+import {useState, useRef, useEffect} from "react"
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [abrirMenu, setabrirMenu]=useState(false)
+let menuref = useRef<HTMLDivElement>(null);
+useEffect(() => {
+    
+    document.addEventListener("click", handleClickOutside, true);
+
+}, []);
+const handleClickOutside = (event: { target: any; }) => {
+    if (menuref.current && !menuref.current.contains(event.target)) {
+        console.log("Clique fora do menu");
+        setabrirMenu(false);
+    } else if (menuref.current) {
+        console.log("Clique dentro do menu");
+    }
+};
+  const [abrirMenu, setabrirMenu] = useState(false);
   return (
     <div className="h-screen flex">
 
-      <div className="flex-col w-[20%] md:w-[8%] bg-blue-500 hidden md:flex lg:w-[14%] xl:w-[10%] ">
+      <div className="flex-col w-[20%] md:w-[8%] bg-sky-700 hidden md:flex lg:w-[14%] xl:w-[10%] ">
         <Menu />
       </div>
         {abrirMenu && (
-          <div className="inset-0 bg-blue-500 z-50 md:hidden w-[20%] shadow-lg p-4">
-            <Menu/>
-          </div>
+          <div className="fixed top-0 left-0 h-full w-[50%] bg-sky-700 z-50 md:hidden shadow-lg p-4" ref={menuref}>
+                  <Menu onClose={() => setabrirMenu(false)} />
+        </div>
         )}
-      <div className="flex-1 flex flex-col bg-gray-50 overflow-auto">
+      <div className="flex-1 flex flex-col  bg-gray-50 overflow-auto">
         <Navbar clicadoMenuAction={() => setabrirMenu(!abrirMenu)} />
         
         <main className="flex-1 p-4 overflow-auto">
