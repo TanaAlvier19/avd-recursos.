07eda09d40d222fd14065071a7c6078561495ee5
TanaAlvier19 <tanaalvier@gmail.com>
2025-06-25 22:16:25 +0100

    ola

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,17 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
-import { HouseIcon, CalendarCheck, Users,ListChecks, LibraryBig, ChartBarBig ,Info, Settings, LogOut} from "lucide-react";
+import { LayoutDashboard,HouseIcon,Menu as MenuIcon, CalendarCheck, Users,ListChecks, LibraryBig, ChartBarBig ,Info, Settings, LogOut} from "lucide-react";
+interface MenuProps {
+  onClose?: () => void;
+}
+
 const menuItems = [
   {
     title: "MENU",
     items: [
       {
-        icon: <HouseIcon />,
+        icon: <LayoutDashboard />,
         label: "Home",
         href: "/admin",
       },
@@ -28,7 +33,7 @@ const menuItems = [
       {
         icon: <LibraryBig />,
         label: "Formações",
-        href: "/list/get_courses",
+        href: "/list/Formacoes",
       },
       {
         icon: <ChartBarBig />,
@@ -46,7 +51,7 @@ const menuItems = [
         href: "/Suporte",
       },
       {
-        icon: <Settings />,
+        icon: <Settings/>,
         label: "Definições",
         href: "/Definições",
       },
@@ -59,29 +64,66 @@ const menuItems = [
   },
 ];
 
-const Menu = () => {
+const Menu = ({ onClose }: MenuProps) => {
   return (
-    <div className="mt-4 text-sm">
-      {menuItems.map((i) => (
-        <div className="flex flex-col gap-2 py-4" key={i.title}>
-          {i.items.map((item) => {
-            return (
-              <Link
+    <div className="h-full flex flex-col justify-between mt-4 text-sm">
+       
+      <div>
+        {onClose && (
+        <div className="flex justify-start gap-2 px-2 mb-4 md:hidden">
+          <MenuIcon
+            onClick={onClose}
+            className="text-white cursor-pointer"
+          />
+          <h1 className="text-white text-lg font-bold">Recursos Humanos</h1>
+
+        </div>
+      )}
+      
+        {menuItems
+          .filter((section) => section.title === "MENU")
+          .map((section) => (
+            <div className="flex flex-col gap-2 py-4" key={section.title}>
+              {section.items.map((item) => (
+                <Link
                   href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start gap-4 text-white py-2 md:px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
+                  className="flex items-center gap-4 text-white py-2 px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
                 >
                   {typeof item.icon === "string" ? (
                     <Image src={item.icon} alt="" width={25} height={25} />
                   ) : (
                     item.icon
                   )}
-                  <span className="hidden lg:block">{item.label}</span>
+                  <span>{item.label}</span>
                 </Link>
-              );
-          })}
-        </div>
-      ))}
+              ))}
+            </div>
+          ))}
+      </div>
+
+      <div>
+        {menuItems
+          .filter((section) => section.title === "OUTROS")
+          .map((section) => (
+            <div className="flex flex-col gap-2 py-4" key={section.title}>
+              {section.items.map((item) => (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center gap-4 text-white py-2 px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
+                >
+                  {typeof item.icon === "string" ? (
+                    <Image src={item.icon} alt="" width={25} height={25} />
+                  ) : (
+                    item.icon
+                  )}
+                  <span>{item.label}</span>
+                </Link>
+              ))}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
