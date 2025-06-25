0a146c44e50d9f677d63adf0c96c37973c6809d6
TanaAlvier19 <tanaalvier@gmail.com>
2025-06-25 22:17:33 +0100

    Update Menu1.tsx

--- src/components/Menu1.tsx
+++ src/components/Menu1.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import { HouseIcon, CalendarCheck, Users,ListChecks, LibraryBig, ChartBarBig ,Info, Settings, LogOut} from "lucide-react";
+import { HouseIcon, CalendarCheck,Menu, Users,ListChecks, LibraryBig, ChartBarBig ,Info, Settings, LogOut, LayoutDashboard } from "lucide-react";
 
+interface MenuProps {
+  onClose?: () => void;
+}
+  
 const menuItems1 = [
   {
     title: "MENU",
     items: [
       {
-        icon: <HouseIcon />,
+        icon: <LayoutDashboard />,
         label: "Home",
         href: "/funcionarios",
       },
       {
-        icon: <CalendarCheck />,
+        icon: <CalendarCheck/>,
         label: "Pedir Dispensas",
         href: "/menu/pedir_dispensa",
-        
       },
       {
         icon: <ListChecks />,
@@ -56,29 +59,63 @@ const menuItems1 = [
   },
 ];
 
-const Menu1 = () => {
+const Menu1 = ({ onClose }: MenuProps) => {
   return (
-    <div className="mt-4 text-sm">
-      {menuItems1.map((i) => (
-        <div className="flex flex-col gap-2 py-4" key={i.title}>
-          {i.items.map((item) => {
-            return (
-              <Link
+    <div className="h-full flex flex-col justify-between mt-4 text-sm">
+       
+      <div>
+        {onClose && (
+        <div className="flex justify-start gap-2 px-2 mb-4 md:hidden">
+          <Menu
+            onClick={onClose}
+            className="text-white cursor-pointer"
+          />
+        </div>
+      )}
+        {menuItems1
+          .filter((section) => section.title === "MENU")
+          .map((section) => (
+            <div className="flex flex-col gap-2 py-4" key={section.title}>
+              {section.items.map((item) => (
+                <Link
                   href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start gap-4 text-white py-2 md:px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
+                  className="flex items-center gap-4 text-white py-2 px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
                 >
                   {typeof item.icon === "string" ? (
                     <Image src={item.icon} alt="" width={25} height={25} />
                   ) : (
                     item.icon
                   )}
-                  <span className="hidden lg:block">{item.label}</span>
+                  <span>{item.label}</span>
                 </Link>
-              );
-          })}
-        </div>
-      ))}
+              ))}
+            </div>
+          ))}
+      </div>
+
+      <div>
+        {menuItems1
+          .filter((section) => section.title === "OUTROS")
+          .map((section) => (
+            <div className="flex flex-col gap-2 py-4" key={section.title}>
+              {section.items.map((item) => (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center gap-4 text-white py-2 px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
+                >
+                  {typeof item.icon === "string" ? (
+                    <Image src={item.icon} alt="" width={25} height={25} />
+                  ) : (
+                    item.icon
+                  )}
+                  <span>{item.label}</span>
+                </Link>
+              ))}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
